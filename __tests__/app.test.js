ab3bbca947683f434ac38a5a804e3ca239c0ca03
Matthew Saber <mattsaber21@yahoo.com>
2021-06-15 11:19:14 -0700

    GET and POST passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,18 +2,15 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('app routes', () => {
+  // eslint-disable-next-line space-before-function-paren
   test('get all from / ', async () => {
-    const color = {
-      body: 'hi',
-      status: '200 OK',
-      contentType: 'text/plain'
-    };
+    // const color = {
+    //   body: 'hi',
+    //   status: '200 OK',
+    //   contentType: 'text/plain'
+    // };
     const res = await request(app)
       .get('/');
-    expect(res.text).toEqual(`HTTP/1.1 200 OK Accept-Ranges: bytes
-Content-Length: 17
-Content-Type: text/plain\r
-\r
-hi');
+    expect(res.text).toEqual('hi');
   });
 });
